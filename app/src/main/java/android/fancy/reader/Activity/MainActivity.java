e1e29992438a100b9c4f4cfbc46b82d851c6b4fc
inX95 <xiezongxin1995@163.com>
2016-07-05 15:02:03 +0800

    初始化materialDesign及相关库环境

--- app/src/main/java/android/fancy/reader/Activity/MainActivity.java
+++ app/src/main/java/android/fancy/reader/Activity/MainActivity.java
@@ -1,5 +1,6 @@
-package android.fancy.reader;
+package android.fancy.reader.Activity;
 
+import android.fancy.reader.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/android/fancy/reader/FancyReaderApplication.java
@@ -0,0 +1,55 @@
+package android.fancy.reader;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.litesuits.orm.LiteOrm;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+/**
+ * Created by inx95 on 16-7-5.
+ */
+public class FancyReaderApplication extends Application {
+    private static FancyReaderApplication applicationContext;
+    private static LiteOrm liteOrm;
+
+    public static void initDB(Context context) {
+        liteOrm = LiteOrm.newCascadeInstance(context, "fancy_reader.db");
+        liteOrm.setDebugged(true);
+    }
+
+    public static void initImageLoader(Context context) {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .denyCacheImageMultipleSizesInMemory()
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+        ImageLoader.getInstance().init(config);
+    }
+
+    public static FancyReaderApplication getInstance() {
+        return applicationContext;
+    }
+
+    public static LiteOrm getLiteOrm() {
+        return liteOrm;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationContext = this;
+        initImageLoader(getApplicationContext());
+        initDB(getApplicationContext());
+    }
+
+    public static SharedPreferences getSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(applicationContext);
+    }
+}
