973751ab5843fbae257a226f5a71cd0ff98ffe62
inX95 <xiezongxin1995@163.com>
2016-07-08 09:23:37 +0800

    建立　Record数据库

--- app/src/main/java/android/fancy/reader/tool/DataSource.java
+++ app/src/main/java/android/fancy/reader/tool/DataSource.java
@@ -3,6 +3,7 @@ package android.fancy.reader.tool;
 import android.content.Context;
 import android.fancy.reader.FancyReaderApplication;
 import android.fancy.reader.bean.Book;
+import android.fancy.reader.bean.Record;
 
 import com.litesuits.orm.LiteOrm;
 
@@ -21,16 +22,34 @@ public class DataSource {
     public static class LiteOrmUtil {
         private static LiteOrm liteOrm= FancyReaderApplication.getLiteOrm();
 
-        public static ArrayList<Book> getBookListFromDB() {
+        //book
+        public static List<Book> getBookListFromDB() {
             return liteOrm.query(Book.class);
         }
-
         public static boolean updateBookListToDB(List<Book> bookList) {
             return liteOrm.update(bookList)==bookList.size();
         }
-
         public static boolean insertBookListToDB(List<Book> bookList) {
             return liteOrm.insert(bookList)==bookList.size();
+        }
+
+        //record
+        public static List<Record> getRecordListFromDB() {
+            return liteOrm.query(Record.class);
+        }
+        public static boolean updateRecordListToDB(List<Record> recordList) {
+            return liteOrm.update(recordList)==recordList.size();
+        }
+        public static boolean insertRecordListToDB(List<Record> recordList) {
+            return liteOrm.insert(recordList)==recordList.size();
+        }
+
+        public static boolean insertRecordToDB(Record record) {
+            return liteOrm.insert(record)==1;
+        }
+
+        public static boolean updateRecordToDB(Record record) {
+            return liteOrm.update(record)==1;
         }
     }
 
